5cb4bec7bfbfe426e7588fa0d1141d573f048d4e
Raghu Rayamajhi <ryamacoding@outlook.com>
2023-10-04 13:47:50 +0100

    javascript challenges re-learn

--- index.js
+++ index.js
@@ -1,143 +1,161 @@
 //* if given a falsy value as element 1 return the falsy value *//
 
-function giveFalsy(elem1, elem2) {
-  return (!elem1) ? elem1 : elem2;
+function returnFalsy(elem1, elem2){
+  return !elem1 ? elem1 : elem2;
 }
 
-console.log(giveFalsy(0, 'dog'))
+console.log(returnFalsy(0, "dog"))
 
 //* return the length of any given array *//
 
-function arrayL(arr) {
-  return arr.length
+function arrayLength(arr){
+    return arr.length
 }
 
-console.log(arrayL([5, 4, 3]))
+console.log(arrayLength([5, 4, 3, 2, 1]))
 
 //* return the last number of any given array *//
 
-function lastArray(arr) {
-  return arr[arr.length - 1]
+function lastNumber(arr) {
+  for (i = 0; i <= arr.length; i++) {
+    return arr[arr.length - 1]
+  }
 }
 
-console.log(lastArray([5, 4, 3, 2]))
+console.log(lastNumber([5, 4, 3, 2, 1]))
 
 //* find the sum of all elements in the array *//
 
-function allElem(arr) {
+function findSum(arr){
   let sum = 0;
 
-  for( i = 0; i < arr.length; i++) {
-    sum = sum + arr[i]
-  }
-
-  return sum
-}
-
-console.log(allElem([5, 4, 3, 2, 1]))
-
-//* add up the numbers from a single number *//
-
-function addNum(num) {
-  let sum = 0;
-
-  for( i = 1; i <= num; i++) {
-    sum = sum + i;
+  for (i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
   }
 
   return sum;
 }
 
-console.log(addNum(3));
+console.log(findSum([5, 4, 3, 2, 1]))
+
+//* add up the numbers from a single number *//
+
+function addNum(num) {
+  let add = 0 
+
+  for (i = 1; i <= num; ++i){
+    add = add + i
+  }
+
+  return add;
+}
+
+console.log(addNum(5))
 
 //* convert seconds into a minute and seconds timer *//
 
-function convertSec(seconds) {
-  let timerMinutes = Math.floor(seconds / 60);
-  let timerSeconds = seconds % 60;
+function convertSeconds(seconds){
 
-  if (timerMinutes.toString().length === 1) {
-    timerMinutes = '0' + timerMinutes
+let TimerSeconds = seconds % 60;
+let TimerMinutes = Math.floor(seconds / 60)
+
+  if(TimerMinutes.toString().length === 1){
+    TimerMinutes = '0' + TimerMinutes;
   }
 
-  return timerMinutes + ':' + timerSeconds
+  if(TimerSeconds.toString().length === 1){
+    TimerSeconds = '0' + TimerSeconds
+  }
+
+  return TimerMinutes + ':' + TimerSeconds;
 }
 
-console.log(convertSec(70))
+console.log(convertSeconds(40))
 
 //* find the biggest number of an element *//
 
-function findBiggest(arr) {
-  let max = arr[0]
+function findNum(arr){
+let num = 0
 
-  for(i = 0; i < arr.length; i++){
-    if(arr [i] > max) {
-      max = arr[i]
+  for (i = 0; i < arr.length; ++i) {
+    if(arr[i] > num) {
+      num = arr[i];
     }
   }
 
-  return max
+  return num;
 }
 
-console.log(findBiggest([5, 10, 20, 12]))
+console.log(findNum([5, 10, 20, 4]))
 
 //* reverse the string abc to cba and so on *//
 
 function reverseString(str) {
-  return str.split('').reverse().join('');
+
+  let reverseString = ""
+
+  for (i = 0; i < str.length; ++i){
+    reverseString = str[i] + reverseString
+  }
+
+  return reverseString;
 }
 
 console.log(reverseString('abc'))
+
 /* break */
 
-function isLoggedIn(str1, str2) {
-  return str1 === 'LOGGED_IN' || str2 === 'SUBSCRIBED';
+function reverseString(str){
+  return str.split("").reverse().join("");
 }
 
-console.log(isLoggedIn('LOGGED_OUT', 'UNSUBSCRIBED'))
+console.log(reverseString('abc'))
 
 //* turn every element of an array into 0*//
 
-function returnZero(arr) {
-let Zero = [];
+function zeroToArray(arr) {
+  let Zero = []
 
-for(i = 0; i < arr.length; i++) {
-  Zero[i] = 0
-}
+  for (i = 0; i < arr.length; ++i){
+    Zero[i] = 0
+  }
+
   return Zero;
 }
 
-console.log(returnZero([9, 10, 11, 12]))
+console.log(zeroToArray([5, 4, 3]))
 
 /* using array.fill*/
 
-function convertToZeros(arr) {
-  return new Array(arr.length).fill(0);
+function convertToZero(arr) {
+    return new Array(arr.length).fill(0);
 }
 
-console.log(convertToZeros([9, 10, 11, 12, 13]))
+console.log(convertToZero([5, 4, 3, 2, 1]))
 
 /* using array.map*/
 
 function toZero(arr) {
   return arr.map(elem => {
-    return 0;
-  })}
+    return 0
+  })
+}
 
-  console.log(toZero([5, 4, 3, 2, 1, 0]))
+console.log(toZero([5, 4, 3, 1]))
+
 
 //* Remove apples *//
 
-function removeApples(arr) {
+function removeApples(arr){
   let noApples = [];
 
-  for (i = 0; i < arr.length; i++) {
+  for(i = 0; i < arr.length; ++i){
     if(arr[i] !== 'Apples') {
-      noApples.push(arr[i])
+      noApples.push(arr[i]);
     }
   }
 
   return noApples;
 }
 
-console.log(removeApples(['Bananas', 'Apples', 'Oranges']))
+console.log(removeApples(['Apples', 'Bananas', 'Oranges']))
